f9bd5fc3cc92e3a312928383338c4a9a98336450
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-02-18 14:49:31 -0700

    1st Commit

---
+++ app/Controllers/ListingsController.js
@@ -0,0 +1,12 @@
+
+
+export default class Listing{
+    constructor(title, imgUrl,description,price,stock){
+        this.title = title,
+        this.imgUrl = imgUrl,
+        this.description = description
+        this.price= price,
+        this.stock = stock
+        
+    }
+}
