0851a595fc760763026f9a19ef7f6cc14204b590
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-18 16:04:34 -0700

    Added cart controller to main.js

--- app/main.js
+++ app/main.js
@@ -1,9 +1,11 @@
-import ValuesController from "./Controllers/ValuesController.js";
+//import ValuesController from "./Controllers/ValuesController.js";
 import ListingsController from "./Controllers/ListingsController.js"
+import CartController from "./Controllers/CartController.js";
 
 class App {
   //valuesController = new ValuesController();
 listingsController = new ListingsController();
+cartController = new CartController();
 }
 
 window["app"] = new App();
