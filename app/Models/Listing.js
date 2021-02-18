b9c2acbe2cba573e7b336580adab5523195ea837
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-18 16:55:02 -0700

    Cart items

--- app/Models/Listing.js
+++ app/Models/Listing.js
@@ -17,14 +17,14 @@ export default class Listing{
     get Template(){
         return /*html*/ `
         <div class="card col-2">
-        <img class="card-img-top" src="${this.imgUrl}" alt="">
-        <div class="card-body">
-            <h4 class="card-title">${this.title}</h4>
-            <p class="card-text">${this.description}</p>
-            <p>Stock : ${this.stock}</p>
-            <p>Price: ${this.price}</p>
-            <button class="btn btn-success"onclick="app.listingsController.addToCart('${this.id}')">Add to Cart</button>
-        </div>
+            <img class="card-img-top" src="${this.imgUrl}" alt="">
+            <div class="card-body">
+                <h4 class="card-title">${this.title}</h4>
+                <p class="card-text">${this.description}</p>
+                <p>Stock : ${this.stock}</p>
+                <p>Price: ${this.price}</p>
+                <button class="btn btn-success"onclick="app.listingsController.addToCart('${this.id}')">Add to Cart</button>
+            </div>
         </div>
         `
     }
--- app/Services/CartService.js
+++ app/Services/CartService.js
@@ -8,7 +8,16 @@ class CartService {
       let cartItems = ProxyState.listings.filter(l => l.numInCart > 0)
       console.log(cartItems);
       cartItems.forEach(l => {
-         result += /*html*/`<div class="col">${l}</div>`
+         result += /*html*/`
+         <div class="card col">
+            <img class="card-img" src="${l.imgUrl}" alt="">
+            <div class="card-body">
+                <h4 class="card-title">${l.title}</h4>
+                <p class="card-text">${l.description}</p>
+                <p>Stock : ${l.numInCart}</p>
+                <p>Price: ${l.price}</p>
+            </div>
+        </div>`
          console.log(l);
       })
       return result
